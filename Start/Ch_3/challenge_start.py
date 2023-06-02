bd001a02bc38b82381cf7dafae159b588b7edc94
Anupam Kumawat <87001738+anupam-kumawat@users.noreply.github.com>
2023-06-02 20:38:38 +0000

    challenge 3 and 4

--- Start/Ch_3/challenge_start.py
+++ Start/Ch_3/challenge_start.py
@@ -1,8 +1,9 @@
 # Example file for Advanced Python: Working With Data by Joe Marini
-# Programming challenge: use advanced data collections on the earthquake data
+# Programming challenge: use advanced data collections on the earthevent data
 
 import json
 import csv
+import datetime
 
 
 # open the data file and load the JSON
@@ -13,3 +14,36 @@ with open("../../30DayQuakes.json", "r") as datafile:
 # 40 most significant seismic events, ordered by most recent
 # Header row: Magnitude, Place, Felt Reports, Date, and Google Map link
 # Date should be in the format of YYYY-MM-DD
+
+
+def getSig(x):
+    sig = x["properties"]["sig"]
+    return 0 if sig is None else sig
+
+
+significantevents = sorted(data["features"], key=getSig, reverse=True)
+significantevents = significantevents[:40]
+significantevents.sort(key=lambda e: e["properties"]["time"], reverse=True)
+
+header = ["Magnitude", "Place", "Felt Reports", "Date", "Google Map Link"]
+rows = []
+
+for event in significantevents:
+    thedate = datetime.date.fromtimestamp(
+        int(event["properties"]["time"])/1000
+    )
+    lat = event["geometry"]["coordinates"][1]
+    long = event["geometry"]["coordinates"][0]
+    gmaplink = f"https://maps.google.com/maps/search/?api=1&query={lat}%2C{long}"
+    rows.append([
+        event["properties"]["mag"],
+        event["properties"]["place"],
+        0 if event["properties"]["felt"] is None else event["properties"]["felt"],
+        thedate,
+        gmaplink
+    ])
+
+with open("challenge.csv", "w") as csvfile:
+    writer = csv.writer(csvfile, delimiter=",")
+    writer.writerow(header)
+    writer.writerows(rows)
--- Start/Ch_4/basiclog_start.py
+++ Start/Ch_4/basiclog_start.py
@@ -1,11 +1,17 @@
 # demonstrate the logging api in Python
 
 # TODO: use the built-in logging module
-
+import logging
 
 # TODO: Use basicConfig to configure logging
-
+logging.basicConfig(level=logging.DEBUG, filename="output.log", filemode="w")
 # TODO: Try out each of the log levels
-
+logging.debug("This is a debug-level message")
+logging.info("This is a info message")
+logging.warning("This is a warning message")
+logging.error("This is a error message")
+logging.critical("This is a critical message")
 # TODO: Output formatted strings to the log
-
+x = "string"
+y = 10
+logging.info(f"Here's a {x} variable and int: {y}")
